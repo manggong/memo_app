c765493d4d163f8a6ea77f0212cab86bfdf8b7b1
manggong <jihwank1995@gmail.com>
2021-02-07 16:22:50 +0900

    init custum Views

---
+++ Mymemory/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  Mymemory
+//
+//  Created by 김지환 on 2021/02/07.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Mymemory/MemoReadVC.swift
@@ -0,0 +1,29 @@
+//
+//  MemoReadVC.swift
+//  Mymemory
+//
+//  Created by 김지환 on 2021/02/07.
+//
+
+import UIKit
+
+class MemoReadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
