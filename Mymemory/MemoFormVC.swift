d6dc219fff29557f367ace518ffc38facf20dbcd
manggong <jihwank1995@gmail.com>
2021-02-07 19:48:27 +0900

    Modify MemoFormVC

--- Mymemory/MemoFormVC.swift
+++ Mymemory/MemoFormVC.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class MemoFormVC: UIViewController {
+class MemoFormVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    var subject: String!
+    
+    @IBOutlet weak var contents: UITextView!
+    @IBOutlet weak var preview: UIImageView!
+    
+    @IBAction func save(_ sender: Any) {
+        
+        guard self.contents.text?.isEmpty == false else {
+            let alert = UIAlertController(
+                title: nil,
+                message: "내용을 입력해 주세요",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true)
+            return
+        }
+        
+        let data = MemoData()
+        
+        data.title = self.subject
+        data.contents = self.contents.text
+        data.image = self.preview.image
+        data.regdata = Date()
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appDelegate.memolist.append(data)
+        
+        _ = self.navigationController?.popViewController(animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func pick(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.allowsEditing = true // 이미지 선택 후 편집 과정을 거침
+        
+        self.present(picker, animated: false, completion: nil)
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        self.preview.image = info[.editedImage] as? UIImage
+        
+        picker.dismiss(animated: false, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        self.contents.delegate = self
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        
+        let contents = textView.text as NSString
+        let length = ((contents.length > 15) ? 15: contents.length)
+        self.subject = contents.substring(with: NSRange(location: 0, length: length))
+        
+        self.navigationItem.title = self.subject
+    }
 }
